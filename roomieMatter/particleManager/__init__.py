a0db4a1d9de954e4467492514b3157d837fefabb
davidwxy <davidwxy@umich.edu>
2023-01-05 23:31:33 -0500

    auto commit

--- roomieMatter/particleManager/__init__.py
+++ roomieMatter/particleManager/__init__.py
@@ -4,7 +4,8 @@ import subprocess
 import os
 
 subprocess.run(["pkill", "-f", "particleManager.py"])
-subprocess.run(["rm", "-f", "var/log/particleManager.log"])
+if os.path.exists("var/log/particleManager.log"):
+    os.remove("var/log/particleManager.log")
 if not os.path.exists("var/log"):
     os.mkdir("var/log")
 subprocess.Popen(["sudo", "python3", "roomieMatter/particleManager/particleManager.py", "--host", 
