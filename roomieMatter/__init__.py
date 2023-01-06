e261db1b7fb6c1a2085bb46376efdb68c28f55a5
davidwxy <davidwxy@umich.edu>
2023-01-05 22:06:38 -0500

    trying again

--- roomieMatter/__init__.py
+++ roomieMatter/__init__.py
@@ -26,5 +26,5 @@ subprocess.run(["pkill", "-f", "particleManager.py"])
 subprocess.run(["rm", "-rf", "var/log/particleManager.log"])
 if not os.path.exists("var/log"):
     os.mkdir("var/log")
-subprocess.Popen(["sudo", "python3", "particleManager/particleManager.py", "--host", 
+subprocess.Popen(["sudo", "python3", "roomieMatter/particleManager/particleManager.py", "--host", 
                   "172.31.28.171", "--port", "1002", "--logfile", "var/log/particleManager.log"])
