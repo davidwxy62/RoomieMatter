1f0ce7434d6c6bf48bf4165f94b8e29fe2803ee3
davidwxy <davidwxy@umich.edu>
2023-01-06 16:12:36 -0500

    auto commit

--- roomieMatter/__init__.py
+++ roomieMatter/__init__.py
@@ -18,5 +18,6 @@ app.config.from_envvar('ROOMIEMATTER_SETTINGS', silent=True)
 import roomieMatter.views  # noqa: E402  pylint: disable=wrong-import-position
 import roomieMatter.model  # noqa: E402  pylint: disable=wrong-import-position
 import roomieMatter.api  # noqa: E402  pylint: disable=wrong-import-position
+import roomieMatter.particleManager  # noqa: E402  pylint: disable=wrong-import-position
 
 
---
+++ roomieMatter/particleManager/__init__.py
@@ -0,0 +1,1 @@
+import roomieMatter.particleManager.particleManager
